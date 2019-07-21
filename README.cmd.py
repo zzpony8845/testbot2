99022dd1a69fa92c2c7ec480b7a4d6b87b55f023
zzpony8845 <53059143+zzpony8845@users.noreply.github.com>
2019-07-21 14:49:03 +0400

    Add files via upload

---
+++ README.cmd.py
@@ -0,0 +1,6 @@
+echo "# testbot " > > > README.md>>
+git init
+git add README.md
+git commit-m "first commit"
+git remote add origin https://github.com/zzpony8845/testbot.git
+git push-U origin master
