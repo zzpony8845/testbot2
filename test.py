5711125e5488338ee8750885bc32b4277f0694ee
zzpony8845 <53059143+zzpony8845@users.noreply.github.com>
2019-07-21 13:56:13 +0400

    Add files via upload

---
+++ test.py
@@ -0,0 +1,18 @@
+import telebot;
+bot = telebot.TeleBot ("919151360:AAHYyDAbeXljm2wnpvSnYaGTzMCGoseF4WE");
+
+# Handles all text messages that contains the commands '/start' or '/help'.
+@bot.message_handler(commands=['start', 'help'])
+def handle_start_help(message):
+	pass
+
+# Handles all sent documents and audio files
+@bot.message_handler(content_types=['document', 'audio'])
+def handle_docs_audio(message):
+	pass
+
+# Handles all text messages that match the regular expression
+@bot.message_handler(regexp="SOME_REGEXP")
+def handle_message(message):
+	pass
+
